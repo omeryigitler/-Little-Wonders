32c730f1e2989e3235380783e6102cef737ebb6d
Omer YIGITLER <138784969+omeryigitler@users.noreply.github.com>
2026-05-23 23:24:24 +0200

    Wire product detail page route

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Storefront from './Storefront';
+import ProductDetailPage from './ProductDetailPage';
 import { AdminLayout } from './admin/AdminLayout';
 import { AdminDashboard } from './admin/AdminDashboard';
 import { AdminProducts } from './admin/AdminProducts';
@@ -8,23 +9,5 @@ import { ProductForm } from './admin/ProductForm';
 import { PersonalizationTemplateManager } from './admin/PersonalizationTemplateManager';
 
 export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Storefront />} />
-        
-        {/* Admin Routes */}
-        <Route path="/admin" element={<AdminLayout />}>
-          <Route index element={<AdminDashboard />} />
-          <Route path="products" element={<AdminProducts />} />
-          <Route path="products/new" element={<ProductForm />} />
-          <Route path="products/:id/edit" element={<ProductForm />} />
-          <Route path="templates" element={<PersonalizationTemplateManager />} />
-          <Route path="*" element={<Navigate to="/admin" replace />} />
-        </Route>
-        
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <BrowserRouter><Routes><Route path="/" element={<Storefront />} /><Route path="/products/:slug" element={<ProductDetailPage />} /><Route path="/admin" element={<AdminLayout />}><Route index element={<AdminDashboard />} /><Route path="products" element={<AdminProducts />} /><Route path="products/new" element={<ProductForm />} /><Route path="products/:id/edit" element={<ProductForm />} /><Route path="templates" element={<PersonalizationTemplateManager />} /><Route path="*" element={<Navigate to="/admin" replace />} /></Route><Route path="*" element={<Navigate to="/" replace />} /></Routes></BrowserRouter>;
 }
